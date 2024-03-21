f71f2cdbf6797694ddb7ae5026e8e56c607c9976
Prakhar Srivastav <prsrivastav@google.com>
2024-03-21 16:34:03 -0700

    delete idx file

--- api-injection/idx-template.nix
+++ api-injection/idx-template.nix
@@ -3,7 +3,6 @@
   bootstrap = ''
     mkdir -p "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
-    cp -rf ${./dev.nix} "$WS_NAME/.idx/dev.nix"
     echo ${apikey} > "$WS_NAME/apikey.txt"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
