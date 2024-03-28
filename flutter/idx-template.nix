b2695b8dc99157bcd8e319af33319462730375ac
Prakhar Srivastav <prsrivastav@google.com>
2024-03-28 11:27:09 -0700

    Update idx-template.nix

--- flutter/idx-template.nix
+++ flutter/idx-template.nix
@@ -3,7 +3,7 @@
   bootstrap = ''
     mkdir -p "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
-    echo ${apikey} > "$WS_NAME/apikey.txt"
+    echo "hello" > "$WS_NAME/hello.txt"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
   '';
