fad525e3d36a107aac4e6656c117e890c0a269ea
Prakhar Srivastav <prsrivastav@google.com>
2024-03-28 11:16:50 -0700

    add nexT

---
+++ nextjs/idx-template.nix
@@ -0,0 +1,24 @@
+{ pkgs, importAlias ? "@/*",  language ? "ts", packageManager ? "npm", srcDir ? false, eslint ? false, app ? false, tailwind ? false, ... }: {
+	
+    packages = [
+			pkgs.nodejs_20
+			pkgs.yarn
+			pkgs.nodePackages.pnpm
+			pkgs.bun
+    ];
+
+    bootstrap = ''
+			mkdir "$out"
+			pnpm create next-app "$out" \
+				--import-alias=${importAlias} \
+				--${language} \
+				--use-${packageManager} \
+				${if eslint then "--eslint" else "--no-eslint" } \
+				${if srcDir then "--src-dir" else "--no-src-dir" } \
+				${if app then "--app" else "--no-app" } \
+				${if tailwind then "--tailwind" else "--no-tailwind" }
+
+			mkdir -p "$out"/.idx
+  		cp ${./dev.nix} "$out"/.idx/dev.nix
+    '';
+}
