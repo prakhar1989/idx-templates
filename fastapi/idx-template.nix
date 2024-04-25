060beb076ea9b6d406cd4d4ed6e213f12c98d68d
Vladimir Kryachko <v.kryachko@gmail.com>
2024-04-25 13:08:44 -0400

    Update idx-template.nix

--- fastapi/idx-template.nix
+++ fastapi/idx-template.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   bootstrap = ''
-    cp -rf ${./.}/app "$WS_NAME"
+    cp -rf ${./app} "$WS_NAME"
     chmod -R +w "$WS_NAME"
     chmod +x "$WS_NAME"/devserver.sh
     mv "$WS_NAME" "$out"
